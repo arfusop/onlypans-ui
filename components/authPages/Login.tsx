0adc2f2f2d3ab6274d8c2082310d641c3e4e9a38
Phil Arfuso <arfusop@gmail.com>
2022-05-26 21:20:54 -0400

    add login form

--- components/authPages/Login.tsx
+++ components/authPages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
 import {
     TextField,
@@ -15,11 +15,69 @@ import {
     Fastfood
 } from '@mui/icons-material'
 
+import { VALID_PASSWORD, VALID_EMAIL } from '../../utilities/regex'
 import styles from './AuthPages.module.scss'
 
+type formValueTypes = {
+    value: string
+    error: string
+}
+
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false)
-    const [rememberMe, setRememberMe] = useState(false)
+    const [email, setEmail] = useState<formValueTypes>({ value: '', error: '' })
+    const [password, setPassword] = useState<formValueTypes>({
+        value: '',
+        error: ''
+    })
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    // TODO: Remember me logic
+    const [rememberMe, setRememberMe] = useState<boolean>(false)
+    const [submitDisabled, setSubmitDisabled] = useState<boolean>(true)
+
+    const setIsFormValid = useCallback(() => {
+        const isEmailValid = email.value && !email.error
+        const isPasswordValid = password.value && !password.error
+
+        if (submitDisabled) {
+            if (isEmailValid && isPasswordValid) {
+                setSubmitDisabled(false)
+            }
+        } else {
+            if (!isEmailValid || !isPasswordValid) {
+                setSubmitDisabled(true)
+            }
+        }
+    }, [email, password, submitDisabled])
+
+    useEffect(() => {
+        setIsFormValid()
+    }, [setIsFormValid])
+
+    const onFieldUpdate = (e: any) => {
+        const { name, value } = e.target
+        switch (name) {
+            case 'email':
+                const newEmailValue = {
+                    value,
+                    error: VALID_EMAIL.test(value)
+                        ? ''
+                        : 'Please enter a valid email'
+                }
+                setEmail(newEmailValue)
+                break
+            case 'password':
+                const newPasswordValue = {
+                    value,
+                    error: VALID_PASSWORD.test(value)
+                        ? ''
+                        : 'Please enter a password with at least 8 alpha-numeric digits, 1 capital letter, 1 number, and 1 special character'
+                }
+                setPassword(newPasswordValue)
+                break
+            default:
+                break
+        }
+    }
 
     return (
         <section className={styles.AuthPage}>
@@ -32,6 +90,11 @@ const Login = () => {
                 <h1>Login</h1>
                 <TextField
                     label="Email"
+                    name="email"
+                    value={email.value}
+                    error={!!email.error}
+                    helperText={email.error}
+                    onChange={onFieldUpdate}
                     InputProps={{
                         endAdornment: (
                             <InputAdornment position="end">
@@ -46,6 +109,11 @@ const Login = () => {
                 />
                 <TextField
                     label="Password"
+                    name="password"
+                    error={!!password.error}
+                    helperText={password.error}
+                    value={password.value}
+                    onChange={onFieldUpdate}
                     type={showPassword ? 'text' : 'password'}
                     InputProps={{
                         endAdornment: (
@@ -81,7 +149,10 @@ const Login = () => {
                         </Link>
                     </span>
                 </div>
-                <Button variant="contained" size="large">
+                <Button
+                    variant="contained"
+                    size="large"
+                    disabled={submitDisabled}>
                     Login
                 </Button>
                 <div className={styles.additionalInfo}>
